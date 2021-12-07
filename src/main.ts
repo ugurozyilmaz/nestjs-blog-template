140b3987233f362876bf953a4c5b540ea4a4f204
Ugur Ozyilmaz <ugurozyilmaz08@gmail.com>
2021-12-07 22:59:52 +0300

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = +process.env.APP_PORT || 3000;
+
+  app.setGlobalPrefix('api');
+  console.log('Port running on: ', port);
+
+  const options = new DocumentBuilder()
+    .setTitle('Blog APP')
+    .setDescription('Blog API documentation')
+    .setVersion('1.0')
+    .addTag('Blog')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
